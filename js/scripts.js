7c64c29654082682d6ef300fb79acbcff1c2b963
Valenn <valenn.iglesias13@gmail.com>
2021-11-29 14:02:56 -0300

    version terminada

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest("[data-dropdown]")
+        currentDropdown.classList.toggle("active")
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(subMenu__iconos => {
+        if (subMenu__iconos === currentDropdown) return
+        subMenu__iconos.classList.remove("active")
+    })
+})
